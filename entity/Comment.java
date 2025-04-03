349e212c17f5dea7d251939fafbf1c8869fcc153
RKS-t <rudtjs5719@gmail.com>
2025-04-03 14:06:27 +0900

    feat : 덧글 기능 entity 추가 및 클래스 생성

---
+++ entity/Comment.java
@@ -0,0 +1,42 @@
+package com.example.scheduleproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "comment")
+public class Comment extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "longtext", nullable = false)
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "Plan_id")
+    private Plan plan;
+
+    public Comment() {
+    }
+
+    public Comment(String contents) {
+        this.contents = contents;
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+    }
+
+    public void setPlan(Plan plan){
+        this.plan = plan;
+    }
+
+
+}
